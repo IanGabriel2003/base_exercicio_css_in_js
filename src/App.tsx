30e219b5e2423be1b3bafcf27ff62a284a6691f9
Ian Gabriel Pereira de Queiroz <iangabrielpq392@gmail.com>
2024-07-26 17:34:57 -0300

    Adicionando styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import Header from './components/Cabecalho'
-import Hero from './components/Hero'
-import ListaVagas from './containers/ListaVagas'
+import React from 'react'
 
-import './global.css'
+import ListaVagas from './containers/ListaVagas'
+import Hero from './components/Hero'
+import Header from './components/Cabecalho'
+
+import { Global } from './styles'
 
 function App() {
   return (
     <>
+      <Global />
       <Header />
       <Hero />
       <div className="container">
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,14 @@
 import styles from './Hero.module.css'
+import { FormDiv, FormHero, HeroTitle } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <FormHero>
+    <FormDiv>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </FormDiv>
+  </FormHero>
 )
 
 export default Hero
